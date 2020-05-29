a71414624ac6fbe924690f15b71c8b69afd72e57
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-29 04:23:05 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp25/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WindowsFormsApp25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Datapr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Srok = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Com = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.way = new System.Windows.Forms.TextBox();
+            this.show = new System.Windows.Forms.Button();
+            this.no_act = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clear_all = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Price,
+            this.Datapr,
+            this.Srok,
+            this.Num,
+            this.Com});
+            this.table.Location = new System.Drawing.Point(12, 84);
+            this.table.Name = "table";
+            this.table.RowHeadersWidth = 51;
+            this.table.RowTemplate.Height = 24;
+            this.table.Size = new System.Drawing.Size(789, 245);
+            this.table.TabIndex = 0;
+            // 
+            // Name
+            // 
+            this.Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Name.HeaderText = "Название";
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
+            this.Name.Width = 101;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Цена";
+            this.Price.MinimumWidth = 6;
+            this.Price.Name = "Price";
+            this.Price.Width = 125;
+            // 
+            // Datapr
+            // 
+            this.Datapr.HeaderText = "Дата производства";
+            this.Datapr.MinimumWidth = 6;
+            this.Datapr.Name = "Datapr";
+            this.Datapr.Width = 125;
+            // 
+            // Srok
+            // 
+            this.Srok.HeaderText = "Срок годности";
+            this.Srok.MinimumWidth = 6;
+            this.Srok.Name = "Srok";
+            this.Srok.Width = 125;
+            // 
+            // Num
+            // 
+            this.Num.HeaderText = "Количество";
+            this.Num.MinimumWidth = 6;
+            this.Num.Name = "Num";
+            this.Num.Width = 125;
+            // 
+            // Com
+            // 
+            this.Com.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Com.HeaderText = "Перечень продуктов";
+            this.Com.MinimumWidth = 6;
+            this.Com.Name = "Com";
+            this.Com.Width = 159;
+            // 
+            // way
+            // 
+            this.way.Location = new System.Drawing.Point(179, 12);
+            this.way.Name = "way";
+            this.way.Size = new System.Drawing.Size(471, 22);
+            this.way.TabIndex = 1;
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(656, 12);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(137, 23);
+            this.show.TabIndex = 2;
+            this.show.Text = "Показать";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // no_act
+            // 
+            this.no_act.Location = new System.Drawing.Point(12, 41);
+            this.no_act.Name = "no_act";
+            this.no_act.Size = new System.Drawing.Size(789, 37);
+            this.no_act.TabIndex = 3;
+            this.no_act.Text = "Просроченый товар";
+            this.no_act.UseVisualStyleBackColor = true;
+            this.no_act.Click += new System.EventHandler(this.no_act_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(12, 335);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(392, 42);
+            this.clear.TabIndex = 4;
+            this.clear.Text = "Сбросить";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите путь к файлу:";
+            // 
+            // clear_all
+            // 
+            this.clear_all.Location = new System.Drawing.Point(409, 335);
+            this.clear_all.Name = "clear_all";
+            this.clear_all.Size = new System.Drawing.Size(392, 42);
+            this.clear_all.TabIndex = 6;
+            this.clear_all.Text = "Очистить все";
+            this.clear_all.UseVisualStyleBackColor = true;
+            this.clear_all.Click += new System.EventHandler(this.clear_all_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 395);
+            this.Controls.Add(this.clear_all);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.no_act);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.way);
+            this.Controls.Add(this.table);
+           // this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.TextBox way;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.Button no_act;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Datapr;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Srok;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Num;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Com;
+        private System.Windows.Forms.Button clear_all;
+    }
+}
+
---
+++ WindowsFormsApp25/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp25
+{
+    public abstract class Tovar
+    {
+        public abstract void info(DataGridViewRow row);
+        public abstract bool actual();
+    }
+
+    public class Product : Tovar
+    {
+        private string name;
+        private string price;
+        private DateTime datepr;
+        private DateTime srok;
+
+        public Product(string name1, string price1, DateTime datepr1, DateTime srok1)
+        {
+            name = name1;
+            price = price1;
+            datepr = datepr1;
+            srok = srok1;
+        }
+
+        public override void info(DataGridViewRow row)
+        {
+            row.Cells["Name"].Value = name;
+            row.Cells["Price"].Value = price;
+            row.Cells["Datapr"].Value = datepr.ToShortDateString();
+            row.Cells["Srok"].Value = srok.ToShortDateString();
+        }
+        public override bool actual()
+        {
+            if (srok > DateTime.Now) return true;
+            else return false;
+        }
+    }
+    public class Part : Tovar
+    {
+        private string name;
+        private string price;
+        private int number;
+        private DateTime datepr;
+        private DateTime srok;
+
+        public Part(string name1, string price1, int number1, DateTime datepr1, DateTime srok1)
+        {
+            name = name1;
+            price = price1;
+            number = number1;
+            datepr = datepr1;
+            srok = srok1;
+        }
+        public override void info(DataGridViewRow row)
+        {
+            row.Cells["Name"].Value = name;
+            row.Cells["Price"].Value = price;
+            row.Cells["Num"].Value = number;
+            row.Cells["Datapr"].Value = datepr.ToShortDateString();
+            row.Cells["Srok"].Value = srok.ToShortDateString();
+        }
+        public override bool actual()
+        {
+            if (srok > DateTime.Now) return true;
+            else return false;
+        }
+    }
+    class Compl : Tovar
+    {
+        private string name;
+        private string price;
+        private string component;
+
+        public Compl() { }
+
+        public Compl(string name1, string price1, string component1)
+        {
+            name = name1;
+            price = price1;
+            component = component1;
+        }
+
+        public override void info(DataGridViewRow row)
+        {
+            row.Cells["Name"].Value = name;
+            row.Cells["Price"].Value = price;
+            row.Cells["Com"].Value = component;
+        }
+        public override bool actual() => true;
+    }
+ 
+    public partial class Form1 : Form
+    {
+        List<Tovar> product_list = new List<Tovar>();
+        private void pr(DataGridView tabel, List<Tovar> list)
+        {
+            tabel.RowCount = list.Count;
+            for (int i = 0; i < tabel.RowCount; i++)
+            {
+                list[i].info(tabel.Rows[i]);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fileName = way.Text;
+                string[] line = File.ReadAllLines(fileName);
+                for (int i = 0; i < line.Length; i++)
+                {
+                    string[] position = line[i].Split(';');
+                    if (position.Length == 4)
+                    {
+                        Product product = new Product(position[0].ToString(), position[1].ToString(), Convert.ToDateTime(position[2]), Convert.ToDateTime(position[3]));
+                        product_list.Add(product);
+                    }
+                    else
+                    {
+                        if (position.Length == 5)
+                        {
+                            Part part = new Part(position[0], position[1], Convert.ToInt32(position[2]), Convert.ToDateTime(position[3]), Convert.ToDateTime(position[4]));
+                            product_list.Add(part);
+                        }
+                        if (position.Length == 3)
+                        {
+                            Compl compl = new Compl(position[0], position[1], position[2]);
+                            product_list.Add(compl);
+                        }
+
+                    }
+                }
+                pr(table, product_list);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void no_act_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Tovar> not_actual = new List<Tovar>();
+                int count = 0;
+                foreach (Tovar var in product_list)
+                {
+                    if (!var.actual()) count++;
+                }
+                if (count != 0)
+                {
+                    foreach (Tovar var in product_list)
+                    {
+                        if (!var.actual()) not_actual.Add(var);
+                    }
+                    pr(table, not_actual);
+                }
+                else MessageBox.Show("Просроченных товаров нет!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            pr(table, product_list);
+        }
+
+        private void clear_all_Click(object sender, EventArgs e)
+        {
+            way.Clear();
+            table.Rows.Clear();
+        }
+    }
+}
